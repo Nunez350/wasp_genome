2e4851734cc579a7e5b661e142463cacb0dd123d
Roy NUNEZ <roynunez@Roys-Air.home>
2017-03-24 12:18:33 -0400

    friday

--- pick-top-hit7.pl
+++ pick-top-hit7.pl
@@ -6,15 +6,14 @@ use List::Util qw(max);
 use Bio::SeqIO;
 #use List::UtilsBy qw(max_by);
 #die "Usage: $0 <Gene call file> <subject file> <BLAST file>\n" unless @ARGV == 1;
-#my ($gene_call, $subject_file, $blast_file) =  @ARGV;
 
 
 my ($blast_file, $gene_call, $anotherSeqFile) =  @ARGV;   #Jeff: I added other sequence file, feel free to change the name
 my %hits2;
 my %hits;
 my $arrayref;    
-   my $count;
-   my $count2;
+my $count;
+my $count2;
 my %uniq;
 
 open BLAST, "<" . $blast_file  or die "Can't open file: $!";
@@ -23,7 +22,7 @@ while ( <BLAST>) {
     chomp;
     my @data = split;
     $uniq{$_}++;    
- 
+    
     my $line = {
         'pident' => $data[2],
         'alignlength' => $data[3],
@@ -32,7 +31,7 @@ while ( <BLAST>) {
 }
 close BLAST;
 print Dumper(\%uniq);
-#################opening gene cal file and storing sequence id and length as key value pairs
+#################opening gene call file and storing sequence id and length as key value pairs
 #we will later divide the sequence length by the drosophila sequence to get the percent that matched.
 
 open GENECALL, "<" . $gene_call;#  or die "Can't open file: $!";
@@ -64,5 +63,5 @@ my %GClen = GCopen($gene_call);
 my %otherLen= GCopen($anotherSeqFile);
 
 
-#lets get this done
+
 #open (my $top_blast, "<", $blast_top) or die "Cannot Open: $blast_top\n";
